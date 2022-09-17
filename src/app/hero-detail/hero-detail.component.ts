728f3a58d9149ebc79d7a8d81919c0eac809c43a
Cristina Nilvan <cristina.nilvan@yahoo.com>
2022-09-17 18:08:12 +0300

    Update hero-detail

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -11,7 +11,7 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./hero-detail.component.css'],
 })
 export class HeroDetailComponent implements OnInit {
-  @Input() hero?: Hero;
+  hero: Hero | undefined;
 
   constructor(
     private route: ActivatedRoute,
